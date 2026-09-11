84672fbf11af552cc37156d09124b160439b643e
Moses <mosesnathanael08@gmail.com>
2026-09-11 10:35:00 +0800

    print the Total Units Processed and theNumber of Failed/Rejected Entries

--- auditor.py
+++ auditor.py
@@ -3,16 +3,20 @@ failed = 0
 while True:
     user = input("Enter Stock Quantity: ")
     if user == "quit":
+        print(f"Total Units Processed: {total_inventory}")
+        print(f"Number of Failed/Rejected: {failed}")
         break  
 
     if user.isdigit() == False:
         print("Error, you should only input integers")
+        failed += 1
         continue
     
     user = int(user)
 
     if user < 0:
         print("Rejected, please input positive number")
+        failed += 1
         continue
 
     total_inventory += 1
